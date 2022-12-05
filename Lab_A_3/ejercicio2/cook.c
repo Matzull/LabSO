f064ef3d62e9743b553c8efa80d486bff4798d85
Marcos <marcal16@ucm.es>
2022-12-05 11:12:44 +0100

    cuc done

--- Lab_A_3/ejercicio2/cook.c
+++ Lab_A_3/ejercicio2/cook.c
@@ -2,9 +2,23 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
+
 #define M 10
+#define SEM_NAME "E2SEM"
+#define SMOBJ_NAME "/sMemoryObj"
 
 int finish = 0;
+int* available_servings;
+sem_t *sem_id;
 
 void putServingsInPot(int servings)
 {
@@ -24,6 +38,38 @@ void handler(int signo)
 
 int main(int argc, char *argv[])
 {
+	//semaphore creation
+	sem_id = sem_open(SEM_NAME, O_CREAT, 0600, 0);
 
+	int fd = 0;//File descriptor for the shared memory obj
+	if((fd = shm_open(SMOBJ_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR)) == -1)
+	{
+		perror("Error, couldnt create memory object\n Exiting...");
+		exit(1);
+	}
+
+	/*Resizing shared memory obj*/
+	ftruncate(fd, sizeof(int));
+
+	/*Remapping shared memory to process virtual memory*/
+	available_servings = (int*)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	printf("%p", available_servings);
+	if (available_servings == MAP_FAILED)
+	{
+		perror("Error, failed mapping memory to virtual memory");
+		exit(1);
+	}
+	
+	
+	printf("Working\n");
+	int num = 5000;
+	memcpy(available_servings, &num, sizeof(int));
+	printf("%d", *available_servings);
+	/*Unmapping shared memory*/
+	munmap(available_servings, sizeof(int));
+	//shm_unlink(SMOBJ_NAME);
+	close(fd);
+	sem_unlink(SEM_NAME);
+	
 	return 0;
 }
--- Lab_A_3/ejercicio2/savages.c
+++ Lab_A_3/ejercicio2/savages.c
@@ -2,11 +2,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+
 #define NUMITER 3
+#define SEM_NAME "E2SEM"
+#define SMOBJ_NAME "/sMemoryObj"
+
+sem_t *sem_id;
+int* available_servings;
 
 int getServingsFromPot(void)
 {
-
+	sem_wait(sem_id);
+	*available_servings--;
+	sem_post(sem_id);
 }
 
 void eat(void)
@@ -18,11 +34,29 @@ void eat(void)
 
 void savages(void)
 {
-
+	for (size_t i = 0; i < NUMITER; i++)
+	{
+		getServingsFromPot();
+		eat();
+	}
+	
 }
 
 int main(int argc, char *argv[])
 {
+
+	//semaphore creation
+	sem_id = sem_open(SEM_NAME, O_CREAT, 0600, 0);
+
+	int fd = 0;//File descriptor for the shared memory obj
+	if( fd = shm_open(SMOBJ_NAME, O_RDWR, 0666) == -1)
+	{
+		perror("Error, couldnt find memory object, please run cook first\n Exiting...");
+		exit(1);
+	}
+	savages();
+
+
 
 	return 0;
 }
